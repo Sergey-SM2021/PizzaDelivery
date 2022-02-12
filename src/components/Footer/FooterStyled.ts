7ad03c59158c45b47696820fc793dfadf3dd8eb3
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-02-12 21:16:30 +0300

    Fix: footer

--- src/components/Footer/FooterStyled.ts
+++ src/components/Footer/FooterStyled.ts
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 export const FooterContent = styled.div`
     display: flex;
+    height: 100%;
     justify-content: space-between;
     align-items: center;
 `
